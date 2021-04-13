0e10ca283f63bcd85000363738e85351be69f364
Wren Rudolph <wren@wrencode.com>
2021-04-13 04:01:25 -0400

    create vue homepage

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Wrencode Logo" src="./assets/sm-fg_hx-brdr_sq.png" />
+  <Home msg="Welcome to the future home of Wrencode, LLC!" />
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+
+export default {
+  name: "App",
+  components: {
+    Home,
+  },
+};
+</script>
+
+<style>
+/* @import url('https://fonts.googleapis.com/css2?family=SourceCodePro&display=swap'); */
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #4a4139;
+  margin-top: 60px;
+}
+
+@font-face {
+  font-family: "Source Code Pro";
+  src: local("SourceCodePro-Regular"),
+    url(./fonts/SourceCodePro/SourceCodePro-Regular.ttf) format("truetype");
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+    <p>
+      Wrencode, LLC focuses on the implementation and optimization of modern
+      technology to help you soar to new heights!
+    </p>
+    <h3>Essential Links</h3>
+    <ul>
+      <li>
+        <a
+          href="https://www.linkedin.com/company/wrencode/"
+          target="_blank"
+          rel="noopener"
+        >
+          Wrencode, LLC on LinkedIn!
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
