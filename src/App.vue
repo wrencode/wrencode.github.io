d1d21cf571339e79e283fe10bf5a2f350d2eb69a
Wren Rudolph <wren@wrencode.com>
2021-04-13 11:27:57 -0400

    add social media links icons and links

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <img alt="Wrencode Logo" src="./assets/sm-fg_hx-brdr_sq.png" />
+  <a href="https://www.wrencode.com">
+    <img alt="Wrencode Logo" src="./assets/images/logo/sm-fg_hx-brdr_sq.png" />
+  </a>
   <Home msg="Welcome to the future home of Wrencode, LLC!" />
 </template>
 
@@ -15,19 +17,23 @@ export default {
 </script>
 
 <style>
-/* @import url('https://fonts.googleapis.com/css2?family=SourceCodePro&display=swap'); */
+/*@import url('https://fonts.googleapis.com/css?family=Source%20Code%20Pro');*/
+
+@font-face {
+  font-family: "Source Code Pro";
+  src: local("Source Code Pro"),
+    url("./assets/fonts/SourceCodePro/SourceCodePro-Regular.ttf")
+      format("truetype");
+  font-weight: normal;
+  font-style: normal;
+}
 
 #app {
+  font-family: "Source Code Pro", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #4a4139;
   margin-top: 60px;
-}
-
-@font-face {
-  font-family: "Source Code Pro";
-  src: local("SourceCodePro-Regular"),
-    url(./fonts/SourceCodePro/SourceCodePro-Regular.ttf) format("truetype");
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,15 +5,70 @@
       Wrencode, LLC focuses on the implementation and optimization of modern
       technology to help you soar to new heights!
     </p>
-    <h3>Essential Links</h3>
+    <h3>Find Wrencode at:</h3>
     <ul>
       <li>
+        <a href="https://github.com/wrencode" target="_blank" rel="noopener">
+          <img
+            src="../assets/images/social-media/github-square.png"
+            alt="Wrencode on GitHub"
+          />
+        </a>
         <a
-          href="https://www.linkedin.com/company/wrencode/"
+          href="https://www.linkedin.com/company/wrencode"
           target="_blank"
           rel="noopener"
         >
-          Wrencode, LLC on LinkedIn!
+          <img
+            src="../assets/images/social-media/linkedin-square.png"
+            alt="Wrencode on LinkedIn"
+          />
+        </a>
+        <a href="https://twitter.com/wrencode" target="_blank" rel="noopener">
+          <img
+            src="../assets/images/social-media/twitter-square.png"
+            alt="Wrencode on Twitter"
+          />
+        </a>
+        <a
+          href="https://www.facebook.com/wrencode"
+          target="_blank"
+          rel="noopener"
+        >
+          <img
+            src="../assets/images/social-media/facebook-square.png"
+            alt="Wrencode on Facebook"
+          />
+        </a>
+        <a
+          href="https://www.instagram.com/wrencode_llc"
+          target="_blank"
+          rel="noopener"
+        >
+          <img
+            src="../assets/images/social-media/instagram-square.png"
+            alt="Wrencode on Instagram"
+          />
+        </a>
+        <a
+          href="https://www.reddit.com/user/wrencode"
+          target="_blank"
+          rel="noopener"
+        >
+          <img
+            src="../assets/images/social-media/reddit-square.png"
+            alt="Wrencode on Reddit"
+          />
+        </a>
+        <a
+          href="https://www.youtube.com/channel/UCyg5T3IhUCZai0AXhmYeeRw"
+          target="_blank"
+          rel="noopener"
+        >
+          <img
+            src="../assets/images/social-media/youtube-square.png"
+            alt="Wrencode on YouTube"
+          />
         </a>
       </li>
     </ul>
@@ -43,6 +98,7 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: #4a4139;
+  margin: 5px;
 }
 </style>
